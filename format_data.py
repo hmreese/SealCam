29bc75989e3d7a3809397d95f432c9bf986353bf
hmreese <hmreese308@gmail.com>
2022-02-16 21:07:09 -0800

    initial commit

--- format_data.py
+++ format_data.py
@@ -1,1 +1,14 @@
+from datetime import date
 
+## Format the data gotten from NOAA into csv
+
+def write_data():
+    today = date.today()
+    filename = str(today) + ".csv"
+    print(filename)
+    # create file for today's data, open for writing
+    f = open(filename, "w")
+    f.write("Data, Data, Data")
+
+if __name__ == "__main__":
+    write_data()
