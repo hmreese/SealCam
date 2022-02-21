e07ae1e3c2933c07e14c855725947135c2d0b6e0
hmreese <hmreese308@gmail.com>
2022-02-21 13:33:41 -0800

    web setup

--- format_data.py
+++ format_data.py
@@ -1,14 +1,28 @@
 from datetime import date
+import csv
 
 ## Format the data gotten from NOAA into csv
 
-def write_data():
+def setup():
     today = date.today()
     filename = str(today) + ".csv"
-    print(filename)
+
     # create file for today's data, open for writing
     f = open(filename, "w")
-    f.write("Data, Data, Data")
+    f.write(str(today) + "\n")
+    f.write("Time Stamp, Water Temperature, Air Temperature, Wind, Tide, Barometric Pressure\n")
+    f.close()
+
+def write_data(wTemp, aTemp, wind, tide, pressure):
+    today = date.today()
+    filename = str(today) + ".csv"
+    
+    f = open(filename, "a")
+    #csvwriter = csv.writer(f)
+    
+    f.write(wTemp + "," + aTemp + "," + wind + "," + tide + "," + pressure + "\n")
+    f.close()
 
 if __name__ == "__main__":
-    write_data()
+    f = setup()
+    write_data("56.7", "56.7", "3.11", "4.3", "1026.2")
