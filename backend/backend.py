6bd560fe3db34da5e140e6b2dd57b0e948334a1e
hmreese <hmreese308@gmail.com>
2022-04-05 17:31:27 -0700

    ahhhhhh

--- backend/backend.py
+++ backend/backend.py
@@ -5,35 +5,16 @@ import hashlib
 import json
 from flask_cors import CORS
 # from mongodb import User
-import backend.get_stuff as get_stuff
+from get_stuff import *
 
 
 app = Flask(__name__)
 CORS(app)
 
-pass_dict = {"hreese": "password", "mdedijer": "password"}
-
 @app.route('/', methods=['GET', 'POST'])
 def helloWorld():
     if request.method == 'GET':
         return jsonify("Welcome to Seal Cam Stats!"), 200
-
-    # log in
-    # elif request.method == 'POST':
-    #     ret = request.get_json()
-    #     try:
-    #         username = ret["username"]
-    #         password = ret["password"]
-    #     except:
-    #         return jsonify({}), 400
-
-    #     if len(password) == 0 or username is None:
-    #         return jsonify({}), 400
-
-    #     if pass_dict[username] != password:
-    #         return jsonify({"username": username}), 400
-
-    #     return jsonify({"username": username}), 200
 
 # home page
 @app.route('/home', methods=['GET', 'POST'])
@@ -47,7 +28,6 @@ def get_home():
         try:
             start = ret["start"]
             end = ret["end"]
-            #time = ret["time"]
         except:
             return jsonify('Bad Request'), 400
 
@@ -55,249 +35,8 @@ def get_home():
         stuff = get_stuff(start, end)
         if stuff == -1:
             return jsonify('Bad Request, Dates must be within 30 days of eachother'), 400
-    
 
-        return jsonify("Data from: {0} - {1}".format(start, end)), 200
-
-    # user = User().find_by_username(username)
-    # if user is None:
-    #     return jsonify("User does not exist"), 400
-
-    # user[0]["password"] = "nope"
-    # return jsonify(user), 200
-
-# @app.route('/api/users', methods=['POST'])
-# def create_user():
-#     if request.method == 'POST':
-#         ret = request.get_json()
-#         try:
-#             username = ret["username"]
-#             password = ret["password"]
-#             name = ret["name"]
-#         except:
-#             return jsonify({}), 418
-
-#         if len(password) == 0 or len(username) == 0 or User().find_by_username(username) is not None:
-#             return jsonify({}), 418
-
-#         hashedPas = hashlib.sha256(password.encode())
-#         user = {'username': username, 'password': str(hashedPas.hexdigest()), 'name': name, 'lists': [], 'friends': []}
-#         newUser = User(user)
-#         newUser.save()
-#         resp = jsonify({"username": username}), 201
-
-#         return resp
-
-# @app.route('/<username>/lists/<listname>', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-# def get_task(username, listname):
-#     if request.method == 'GET':
-#         user = User().find_by_username(username)
-#         if user is None:
-#             return jsonify("User does not exist"), 400
-
-#         lists = user[0]["lists"]
-
-#         for l in lists:
-#             if l["name"] == listname:
-#                 return jsonify(l["tasks"]), 200
-
-#         return jsonify({}), 400
-
-#     elif request.method == 'POST':
-#         try:
-#             title = request.get_json()['title']
-#             date = request.get_json()['date']
-#             description = request.get_json()['description']
-#             priority = request.get_json()['priority']
-#         except:
-#             return jsonify({}), 400
-
-#         ret = User().add_task(username, listname, title, date, description, priority)
-#         user = User().find_by_username(username)
-#         lists = user[0]["lists"]
-#         return jsonify(lists), 201
-
-#     elif request.method == 'PATCH':
-#         try:
-#             completed = request.get_json()['completed']
-#             task_num = request.get_json()['task_num']
-#         except:
-#             return jsonify({}), 400
-
-#         ret = User().complete_task(username, listname, task_num, completed)
-#         user = User().find_by_username(username)
-#         lists = user[0]["lists"]
-#         return jsonify(lists), 201
-
-#     elif request.method == 'DELETE':
-#         try:
-#             task_num = request.get_json()['task_num']
-#         except:
-#             return jsonify({}), 400
-
-#         ret = User().remove_task(username, listname, task_num)
-#         user = User().find_by_username(username)
-#         lists = user[0]["lists"]
-#         return jsonify(lists), 200
-
-
-# @app.route('/<username>/lists', methods=['GET', 'POST', 'PATCH', 'DELETE'])
-# def get_lists(username):
-#     user = User().find_by_username(username)
-#     if user is None:
-#         return jsonify("User does not exist"), 400
-
-#     lists = user[0]["lists"]
-
-#     if request.method == 'GET':
-#         return jsonify(lists), 200
-
-#     elif request.method == 'POST':
-#         try:
-#             listname = request.get_json()['listname']
-#         except:
-#             return jsonify({}), 400
-#         try:
-#             public = request.get_json()['public']
-#         except:
-#             public = False
-
-#         for l in lists:
-#             if l["name"] == listname:
-#                 ret = User().update_list(username, listname, public)
-#                 return jsonify(lists)
-
-#         ret = User().add_list(username, listname, public)
-#         user = User().find_by_username(username)
-#         lists = user[0]["lists"]
-#         return jsonify(lists), 201
-
-#     elif request.method == 'PATCH':
-#         try:
-#             listname = request.get_json()['listname']
-#         except:
-#             return jsonify({}), 400
-#         try:
-#             public = request.get_json()['public']
-#             ret = User().update_list_public(username, listname, public)
-#             user = User().find_by_username(username)
-#             lists = user[0]["lists"]
-#             return jsonify(lists), 200
-#         except:
-#             public = None
-#         try:
-#             completed = request.get_json()['completed']
-#             ret = User().update_list_completed(username, listname, completed)
-#             user = User().find_by_username(username)
-#             lists = user[0]["lists"]
-#             return jsonify(lists), 200
-#         except:
-#             completed = None
-
-
-#     elif request.method == 'DELETE':
-#         try:
-#             listname = request.get_json()['listname']
-#         except:
-#             return jsonify({}), 400
-#         if username is None or len(listname) == 0:
-#             return jsonify({}), 400
-
-#         ret = User().remove_list(username, listname)
-#         user = User().find_by_username(username)
-#         lists = user[0]["lists"]
-#         return jsonify(lists), 200
-
-
-# @app.route('/<username>/friends',  methods=['GET', 'POST'])
-# def get_friends(username):
-#     if request.method == 'GET':
-#         friendList = []
-#         user = User().find_by_username(username)
-#         if user is None:
-#             return jsonify("User does not exist"), 400
-
-#         lists = user[0]["friends"]
-#         for i in lists:
-#             fren = User().find_by_username(i)
-#             friendList.append(fren)
-#         return jsonify(friendList), 200
-
-#     elif request.method == 'POST':
-#         try:
-#             fUsername = request.get_json()['friend_username']
-#         except:
-#             return jsonify({}), 400
-#         ret = User().add_friend(username, fUsername)
-#         user = User().find_by_username(username)
-#         lists = user[0]["friends"]
-#         friendList = []
-#         for i in lists:
-#             fren = User().find_by_username(i)
-#             friendList.append(fren)
-#         return jsonify(friendList), 200
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def helloWorld():
-#     if request.method == 'GET':
-#         return jsonify('Hello, World!'), 200
-
-#     elif request.method == 'POST':
-#         ret = request.get_json()
-#         try:
-#             username = ret["username"]
-#             password = ret["password"]
-#         except:
-#             return jsonify({}), 400
-
-#         print(len(password),len(username))
-#         if len(password) == 0 or username is None:
-#             return jsonify({}), 400
-#         hashedPas = hashlib.sha256(password.encode())
-
-#         resp = User().find_by_username(username)
-
-#         if resp == [] or resp[0]['password'] != hashedPas.hexdigest():
-#             return jsonify({"username": username}), 400
-
-#         return jsonify({"username": username}), 200
-
-
-# @app.route('/api/users', methods=['POST'])
-# def create_user():
-#     if request.method == 'POST':
-#         ret = request.get_json()
-#         try:
-#             username = ret["username"]
-#             password = ret["password"]
-#             name = ret["name"]
-#         except:
-#             return jsonify({}), 418
-
-#         if len(password) == 0 or len(username) == 0 or User().find_by_username(username) is not None:
-#             return jsonify({}), 418
-
-#         hashedPas = hashlib.sha256(password.encode())
-#         user = {'username': username, 'password': str(hashedPas.hexdigest()), 'name': name, 'lists': [], 'friends': []}
-#         newUser = User(user)
-#         newUser.save()
-#         resp = jsonify({"username": username}), 201
-
-#         return resp
-
-
-# @app.route('/admin', methods=['GET'])
-# def admin_stats():
-#     if request.method == 'GET':
-#         resp = User().find_all()
-#         numusers = len(resp)
-#         count = 0
-#         for i in resp:
-#             count += len(i["lists"])
-
-#         done = jsonify({"number_of_users": numusers, "number_of_lists": count}), 200
-#         return done
+        return stuff, 200
 
 
 if __name__ == "__main__":
--- backend/get_stuff.py
+++ backend/get_stuff.py
@@ -1,80 +1,34 @@
-from selenium import webdriver
-import datetime
-from selenium.webdriver.chrome.options import Options
-from datetime import timedelta
-from pytz import timezone
 from datetime import datetime
+from datetime import timedelta
 
 import pprint
 import requests
-import json
 
 def get_stuff(startDate, endDate):
-    # loop=1
-    # while loop == 1:
-        #startDate = input("Start Date (YYYY-MM-DD): ")
-        #endDate = input("End Date (YYYY-MM-DD): ")
-        #days = int(input("How many days apart are the dates? "))
+
     d1 = datetime.strptime(startDate, "%Y-%m-%d")
     d2 = datetime.strptime(endDate, "%Y-%m-%d")
-    days = abs((d2 - d1).days)
-    if(days > 30):
+    
+    numdays = abs((d2 - d1).days) + 1
+    
+    if(numdays > 30):
         print("Too many days apart, must be 30 days or less")
         return -1
-        #readTime = input("Time (HH:MM): ")
-        # if(days <= 30):
-        #     loop = 0
-
-    # s = "00:00"
-    # e = "23:00"
-    # ct = endDate + " " + e
-
-    #it only has a 30 day range so the begin date needs to constantly shift
-    #bd = startDate + " " + s
-    # begin_date = bd[0:4]+bd[5:7]+bd[8:10] #+'+'+bd[11:16]
-    # end_date = ct[0:4]+ct[5:7]+ct[8:10]   #+'+'+ct[11:16]
-    begin_date = startDate.replace('-', '')
-    end_date = startDate.replace('-', '')
-
-    #curr_time = (current_time - datetime.timedelta(minutes=30)).time()
-    # curr_t = s
-
-    # parse_dt = ct[:10] + " " + curr_t[:5]
-
-    # forparse = parse_dt[:16]
-    #need to make sure minutes are a multiple of 6
-    # minutes = int(parse_dt[14:16])
-    # min_diff = int(parse_dt[14:16])%6
-
-    # if min_diff != 0:
-    #     forparse = parse_dt[:14]+str("{0:0=2d}".format(minutes-min_diff))
-    #     readTimeFP = readTime[:3]+str("{0:0=2d}".format(minutes-min_diff))
-    #     begin_date = bd[0:4]+bd[5:7]+bd[8:10]+'+'+readTimeFP
-    #     end_date = ct[0:4]+ct[5:7]+ct[8:10]+'+'+readTimeFP
-
-    # else:
-    #     readTimeFP = readTime
-
-
-    # options = Options()
-    # options.headless = True
-    # path = "/Users/hannahreese/Downloads/chromedriver"
-    # browser = webdriver.Chrome(options=options, executable_path=path)
-    #air_gap, air_pressure, air_temperature, conductivity, currents, currents_survey, currents_predictions, daily_mean, datums, high_low, hourly_height, humidity, monthly_mean, one_minute_water_level, predictions, salinity, visibility, water_level, water_temperature, and wind
-    data_type = ["air_temperature", "air_pressure", "water_temperature"]
-    urls = [" ", " ", " "]
-
-    times = ["00:00", "01:00", "02:00", ]
-
-    for i in range(3):
-        urls[i] = "https://tidesandcurrents.noaa.gov/api/datagetter?product="+data_type[i]+"&begin_date="+begin_date+"&end_date="+end_date+"&interval=h&station=9412110&time_zone=LST_LDT&units=english&format=json&application=NOS.COOPS.TAC.STATIONHOME" 
 
     stuff_dict = {}
 
     # Create a dict for each day, add dicts of each data type with their hourly data
-    for i in range(days + 1):
-        day = int(begin_date) + i
-        print("Date: {0}".format(day))
+    for i in range(numdays):
+        date = (d1 + timedelta(days=i)).date()      # each day
+        date = str(date).replace('-', '')           # format
+        print(date)
+        data_type = ["air_temperature", "air_pressure", "water_temperature"]        # data values
+        urls = [" ", " ", " "]
+
+        # Prepare urls for each data type
+        for k in range(len(data_type)):
+            urls[k] = "https://tidesandcurrents.noaa.gov/api/datagetter?product="+data_type[k]+"&begin_date="+date+"&end_date="+date+"&interval=h&station=9412110&time_zone=LST_LDT&units=english&format=json&application=NOS.COOPS.TAC.STATIONHOME" 
+
         t = ""
         temp = {}
 
@@ -84,30 +38,14 @@ def get_stuff(startDate, endDate):
             data = resp.json()["data"]
             temp[data_type[j]] = []
             for p in data:
-                temp[data_type[j]].append({"t": str(p["t"]).replace('u', '')[11:], "v": str(p["v"]).replace('u', '')})
+                temp[data_type[j]].append({"time": str(p["t"]).replace('u', '')[11:], "value": str(p["v"]).replace('u', '')})
             
         t = str(data[0]["t"]).replace('u', '').replace(' 00:00', '')
         stuff_dict[t] = temp
 
     pprint.pprint(stuff_dict)
+    return stuff_dict
 
-    # for i in range(days + 1):
-    #     forparse_int = int(startDate[8:])+i
-    #     forparse = startDate[:8]+str("{0:0=2d}".format(forparse_int)) #+" "+readTimeFP
-    #     print("Date and Time: ", forparse)
-    #     for j in range(3):
-    #         for k in times:
-    #             dtime = forparse + " " + k
-    #             browser.get(urls[j])
-    #             HTML = browser.page_source
-    #             index = HTML.find(dtime)
-    #             print_out = HTML[index:index+42]
-    #             index = print_out.find('"v":')
-    #             data = print_out[index+5:index+9]
-    #             print("   ",data_type[j], data)
-    #     print(" ")
-
-    # browser.quit()
 
 if __name__ == "__main__":
     s = "2022-04-01"
