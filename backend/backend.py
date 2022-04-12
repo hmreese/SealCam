6077b42cf082318407b62697f689c12476318811
hmreese <hmreese308@gmail.com>
2022-04-12 13:15:11 -0700

    download working! json still messy

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from lib2to3.pgen2.token import AT
+from flask import Flask, make_response, send_file
 from flask import request
 from flask import jsonify
+#from flask_mail import Mail
 import hashlib
 import json
 from flask_cors import CORS
 # from mongodb import User
 from get_stuff import *
-
+import csv
+import pprint
+#import pandas
+#from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 CORS(app)
+#mail = Mail(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def helloWorld():
@@ -36,7 +42,82 @@ def get_home():
         if stuff == -1:
             return jsonify('Bad Request, Dates must be within 30 days of eachother'), 400
 
+        # write to file
+        f = open('sealcamdata.csv', 'w')
+        writer = csv.writer(f)
+        header = ["Date", "Time", "Air Temperature (F)", "Air Pressure (mb)", "Water Temperature (F)"]
+        writer.writerow(header)
+        
+        #pprint.pprint(stuff)
+
+        for datetime in stuff:
+            date = datetime[:10]
+            time = datetime[11:]
+            aTemp = stuff[datetime]["air_temperature"]
+            aPress = stuff[datetime]["air_pressure"]
+            wTemp = stuff[datetime]["water_temperature"]
+            
+            row = [date, time, aTemp, aPress, wTemp]
+            print(row)
+            writer.writerow(row)
+            
+        #pandas.read_csv("sealcamdata.csv")
+
         return stuff, 200
+
+# download page
+@app.route('/download', methods=['GET', 'POST'])
+def get_csv():
+    if request.method == 'GET':
+        # f = open('sealcamdata.csv', 'r')
+        # reader = csv.reader(f)
+        # csv = reader
+        # response = make_response(csv)
+        # cd = 'attachment; filename=sealcamdata.csv'
+        # response.headers['Content-Disposition'] = cd
+        # response.mimetype = 'text/csv'
+
+        # return response
+        return send_file('sealcamdata.csv', as_attachment=True)
+
+#     if request.method == 'POST':
+#         stuff = request.get_json()
+
+#         f = open('sealcamdata.csv', 'w')
+#         writer = csv.writer(f)
+#         header = ["Date", "Time", "Air Temperature (F)", "Air Pressure (mb)", "Water Temperature (F)"]
+#         writer.writerow(header)
+
+#         stuff = sort_it(stuff)
+        
+#         for datetime in stuff:
+#             date = datetime[:10]
+#             time = datetime[11:]
+#             aTemp = stuff[datetime]["air_temperature"]
+#             aPress = stuff[datetime]["air_pressure"]
+#             wTemp = stuff[datetime]["water_temperature"]
+            
+#             row = [date, time, aTemp, aPress, wTemp]
+#             writer.writerow(row)
+
+#         return jsonify("hello"), 200
+
+# def sort_it(stuff):
+#     # convert out of order json/dictionary to list of dicts
+
+#     # find first date
+#     date = datetime.now()
+#     for dt in stuff:
+#         if dt[11:] == "00:00":
+#             dmy = datetime.strptime(str(dt[:10]), "%Y-%m-%d") 
+#             if dmy <= date:
+#                 date = dmy
+    
+#     ret = [{date: stuff[date]}]
+#     for d in stuff:
+#         if
+
+
 
 
 if __name__ == "__main__":
--- backend/get_stuff.py
+++ backend/get_stuff.py
@@ -17,6 +17,7 @@ def get_stuff(startDate, endDate):
 
     stuff_dict = {}
 
+
     # Create a dict for each day, add dicts of each data type with their hourly data
     for i in range(numdays):
         date = (d1 + timedelta(days=i)).date()      # each day
@@ -29,21 +30,34 @@ def get_stuff(startDate, endDate):
         for k in range(len(data_type)):
             urls[k] = "https://tidesandcurrents.noaa.gov/api/datagetter?product="+data_type[k]+"&begin_date="+date+"&end_date="+date+"&interval=h&station=9412110&time_zone=LST_LDT&units=english&format=json&application=NOS.COOPS.TAC.STATIONHOME" 
 
-        t = ""
-        temp = {}
 
         # Create a dict for each data type, add the hourly data
-        for j in range(3):
-            resp = requests.get(url=urls[j])
-            data = resp.json()["data"]
-            temp[data_type[j]] = []
-            for p in data:
-                temp[data_type[j]].append({"time": str(p["t"]).replace('u', '')[11:], "value": str(p["v"]).replace('u', '')})
-            
-        t = str(data[0]["t"]).replace('u', '').replace(' 00:00', '')
-        stuff_dict[t] = temp
+        # air_temperature
+        resp = requests.get(url=urls[0])
+        data = resp.json()["data"]
+        for d in data:
+            t = str(d["t"]).replace('u', '')
+            stuff_dict[t] = {"air_temperature": str(d["v"]).replace('u', '')}
 
-    pprint.pprint(stuff_dict)
+        # the rest
+        i = 1
+        while i < len(data_type):
+            resp = requests.get(url=urls[i])
+            data = resp.json()["data"]
+            for d in data:
+                t = str(d["t"]).replace('u', '')
+                stuff_dict[t][data_type[i]] = str(d["v"]).replace('u', '')
+            i+=1
+
+            # # air_pressure
+            # resp = requests.get(url=urls[2])
+            # data = resp.json()["data"]
+            # for d in data:
+            #     t = str(d["t"]).replace('u', '')
+            #     stuff_dict[t]["water_temperature"] = str(d["v"]).replace('u', '')
+
+
+    #pprint.pprint(stuff_dict)
     return stuff_dict
 
 ## JSON format for stuff_dict:
@@ -71,8 +85,6 @@ def get_stuff(startDate, endDate):
 #   }
 # }
 
-# send spreadsheet options to Avery
-
 # change to for ease of csv conversion:
 # stuff = {
 #   'YYYY-MM-DD HH:MM': {
@@ -87,8 +99,9 @@ def get_stuff(startDate, endDate):
 #   }
 # }
 
+# Date, Time, Data1, Data2, Data3
 
 if __name__ == "__main__":
-    s = "2022-04-01"
-    e = "2022-04-02"
+    s = "2022-04-06"
+    e = "2022-04-07"
     get_stuff(s, e)
