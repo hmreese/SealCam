f58ebbbb2d9039532925ce566d92e59c5cecba71
Mina Dedijer <49251561+minadedijer@users.noreply.github.com>
2022-04-12 14:08:44 -0700

    Delete Frontend directory

--- Frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import MyApp from './MyApp'
-import './index.css'
-
-ReactDOM.render(<MyApp />, document.getElementById('root'))
-
