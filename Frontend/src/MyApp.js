1db29b8fde01ecd5adc50092c0e9b53b7dc094fa
minadedijer <mdedijer@calpoly.edu>
2022-04-25 17:41:55 -0700

    working on linking

--- Frontend/src/MyApp.js
+++ Frontend/src/MyApp.js
@@ -1,7 +1,95 @@
-import React from 'react'
+//import React from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
+//import CORS from 'flask_cors';
+//import CORS from 'flask-cors';
+import axios from 'axios';
 //import TextField from '@mui/material/TextField';
 
+const formReducer = (state, event) => {
+        return {
+          ...state,
+          [event.name]: event.value
+        }
+       }
+
+
+
+async function makePostCall(date){
+        try {
+           const response = await axios.post('http://localhost:5000/home', date);
+           return response;
+        }
+        catch (error) {
+           console.log(error);
+           return false;
+        }
+}
+   
+/*
+function updateList(date) { 
+        makePostCall(date).then( result => {
+        if (result)
+           setData([date]);
+        });
+     }
+*/   
 function MyApp() {
+   
+   //const [data, setData] = useState([{}])
+   const [formData, setFormData] = useReducer(formReducer, {});
+   const [submitting, setSubmitting] = useState(false);
+   
+   useEffect(() => {
+        fetch("/home").then(
+                res => res.json()
+        ).then(
+                data => {
+                        formData(data)
+                        console.log(data)
+                }
+        )
+   }, [])
+
+   const handleSubmit = event => {
+           event.preventDefault();
+           setSubmitting(true);
+           setTimeout(() => { setSubmitting(false);}, 3000)
+           //alert('You have submitted the dates.')
+   }
+
+   const handleChange = event => {
+        setFormData({
+          name: event.target.name,
+          value: event.target.value,
+        });
+      }
+
+   return (
+	<div>
+        <h1>Seal Cam Data</h1>
+        {submitting &&
+            <div>
+                You are submitting the following:
+                <ul>
+                   {Object.entries(formData).map(([name, value]) => (
+                      <li key={name}><strong>{name}</strong> {value}</li>,
+                      <li key={name}><strong>{name}</strong> {value}</li>
+                      ))}
+                </ul>
+            </div>}
+	<p> Submit dates for NOAA data </p>
+	<form onSubmit={handleSubmit}>
+           <fieldset>
+              <label> Start Date </label>
+              <input type="date" name="start" onChange={handleChange}/>
+	      <label> End Date </label>
+              <input type="date" name="end" onChange={handleChange}/>
+           </fieldset>
+           <button type="submit">Submit</button>
+        </form>
+	</div>
+    );
+    /*
    return (
 	<div>
         <h1>Seal Cam Data</h1>
@@ -13,6 +101,7 @@ function MyApp() {
         <input type="submit" value="Submit"/>
 	</div>
     );
+    */
 }
 
 export default MyApp;
